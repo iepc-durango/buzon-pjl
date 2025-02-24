035d0df7f611d75e149b396f829b016fe7deb530
jhovanacarrillo <jhovanacarrillo8@gmail.com>
2025-02-24 12:19:27 -0600

    cambios

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -89,7 +89,9 @@ class NotificacionController extends Controller
             $templateId = $pdf->importPage(1);
             $pdf->useTemplate($templateId);
 
-            $pdf->SetFont('Helvetica', '', 12);
+            
+
+            $pdf->SetFont('Helvetica', '', 11);
 
             $currentDate = now()->format('Y-m-d H:i:s');
             $typeText = $type === 'Acuerdo' ? 'Acuerdo' : 'PES';
@@ -105,13 +107,63 @@ class NotificacionController extends Controller
                 $pdf->Cell(0, 10, $data['titulo'] ?? '', 0, 1);
                 $pdf->SetXY(50, 80);
                 $pdf->MultiCell(0, 10, $data['descripcion'] ?? '', 0, 1);
+
+
+
             } else if ($type === 'PES') {
-                $pdf->SetXY(10, 10);
+                $pdf->SetXY(130, 25);
+                $pdf->Cell(0, 10, $data['tipo'] ?? '', 0, 1);
+                $pdf->SetXY(130, 18);
                 $pdf->Cell(0, 10, $data['no_expediente'] ?? '', 0, 1);
+                $pdf->SetXY(127, 25);
+                $pdf->Cell(0, 10, $data['denunciante'] ?? '', 0, 1);
+                $pdf->SetXY(127, 33);
+                $pdf->Cell(0, 10, $data['denunciado'] ?? '', 0, 1);
+                $pdf->SetXY(20, 98);
+                $pdf->Cell(0, 10, $data['municipio'] ?? '', 0, 1);
+                $pdf->SetXY(20, 115);
+                $pdf->MultiCell(0, 5, $data['descripcion_fundamento'] ?? '', 0, 1);
+                $pdf->SetXY(20, 170);
+                $pdf->MultiCell(0, 5, $data['descripcion_docu'] ?? '', 0, 1);
+                $pdf->SetXY(20, 208);
+                $pdf->MultiCell(0, 5, $data['frag_doc'] ?? '', 0, 1);
+                $pdf->SetXY(10, 10);
+                $pdf->Cell(0, 10, $data['descripcion_notificado'] ?? '', 0, 1);
+                $pdf->SetXY(10, 10);
+
+
+                setlocale(LC_TIME, 'es_ES.UTF-8', 'Spanish_Spain.1252');
+
+                $dia = date('d'); // Día en número
+                $mes = strftime('%B');
+                $mes = ucfirst(strftime('%B')); // Nombre del mes con la primera letra en mayúscula
+                $anio = date('Y'); // Año
+              
+
+                $pdf->SetXY(70, 98);
+                $pdf->Cell(0, 10, "Durango, a", 0, 0);
+
+                $pdf->SetXY(20, 98);
+                $pdf->Cell(0, 10, $dia, 0, 0, 'C'); // Día en número
+
+                $pdf->SetXY(115, 98);
+                $pdf->Cell(0, 10, "de", 0, 0);
+
+                $pdf->SetXY(70, 98);
+                $pdf->Cell(0, 10, $mes, 0, 0, 'C'); // Nombre del mes
+
+                $pdf->SetXY(150, 98);
+                $pdf->Cell(0, 10, "de", 0, 0);
+
+                $pdf->SetXY(160, 98);
+                $pdf->Cell(0, 10, $anio, 0, 0, 'C'); // Año
+                
+
+                
             }
 
-            $pdf->SetXY(10, 290);
-            $pdf->Cell(0, 10, "Tipo: $typeText - Fecha de generación: $currentDate", 0, 1, 'C');
+            //$pdf->SetXY(10, 290);
+            //$pdf->Cell(0, 10, "Tipo: $typeText - Fecha de generación: $currentDate", 0, 1, 'C');
 
             $pdfOutput = $pdf->Output('S');
 
