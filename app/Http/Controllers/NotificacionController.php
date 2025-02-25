55b1218417aa651d06c3bcf8b1768fe9f01d0459
Alondra Gutierrez Flores <alondra.gutierrez.is@unipolidgo.edu.mx>
2025-02-25 10:02:16 -0600

    Update

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -110,6 +110,8 @@ class NotificacionController extends Controller
 
 
 
+
+            } else if ($type === 'PES') {
             } else if ($type === 'PES') {
                 $pdf->SetXY(130, 25);
                 $pdf->Cell(0, 10, $data['tipo'] ?? '', 0, 1);
@@ -158,6 +160,12 @@ class NotificacionController extends Controller
                 $pdf->SetXY(160, 98);
                 $pdf->Cell(0, 10, $anio, 0, 0, 'C'); // Año
                 
+
+                
+            }
+
+
+                setlocale(LC_TIME, 'es_ES.UTF-8', 'Spanish_Spain.1252');
 
                 
             }
